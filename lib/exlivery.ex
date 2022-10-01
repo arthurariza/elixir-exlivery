e9b68f8504c67ef88d26ea49c0af50911b83ba0a
arthurariza <tucaariza@gmail.com>
2022-10-01 19:38:49 -0300

    add Order Facade

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
-  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
   alias Exlivery.Users.UserAgent
+  alias Exlivery.Orders.OrderAgent
+  alias Exlivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrders
 
   def start_agents do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_orders(params), to: CreateOrUpdateOrders, as: :call
 end
